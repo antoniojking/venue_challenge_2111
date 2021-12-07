09be7226c08518591d3c41fecf3f0656df9d81c9
Antonio King <antonio.j.king@gmail.com>
2021-12-07 15:06:56 -0700

    Complete iteration 1

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,11 @@
 class Venue
+  attr_reader :name,
+              :capacity,
+              :patrons
+
+  def initialize(name, number)
+    @name = name
+    @capacity = number
+    @patrons = []
+  end
 end
