8420da3163dcf769dfc1948223b5a0cd4726066d
Antonio King <antonio.j.king@gmail.com>
2021-12-07 15:16:07 -0700

    Complete iteration 2

--- lib/venue.rb
+++ lib/venue.rb
@@ -8,4 +8,14 @@ class Venue
     @capacity = number
     @patrons = []
   end
+
+  def add_patron(person)
+    @patrons << person
+  end
+
+  def yell_at_patrons
+    @patrons.map do |patron|
+      patron.upcase
+    end
+  end
 end
